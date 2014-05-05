e22b4d5e3c2c01894af6f557617d002e0d2c0bfa
andrewgtv <batmangtv@gmail.com>
2014-05-05 17:51:57 +0300

    1

---
+++ ProjectX/src/charen/gtv/projectx/Objects/Defender.java
@@ -0,0 +1,10 @@
+package charen.gtv.projectx.Objects;
+
+public class Defender extends Unit {
+
+	public Defender(int x, int y, int team) {
+		super(1, x, y, team);
+		moveRadius = 1;
+	}	
+	
+}
---
+++ ProjectX/src/charen/gtv/projectx/PictureContainer.java
@@ -0,0 +1,22 @@
+package charen.gtv.projectx;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+
+public class PictureContainer {
+	
+	public Bitmap soldierPicture1;
+	public Bitmap soldierPicture2;
+	public Bitmap defenderPicture1;
+	public Bitmap defenderPicture2;
+
+	public PictureContainer(Resources resources) {
+		soldierPicture1 = BitmapFactory.decodeResource(resources, charen.gtv.projectx.R.drawable.soldier_1);
+		soldierPicture2 = BitmapFactory.decodeResource(resources, charen.gtv.projectx.R.drawable.soldier_2);
+		defenderPicture1 = BitmapFactory.decodeResource(resources, charen.gtv.projectx.R.drawable.defender_1);
+		defenderPicture2 = BitmapFactory.decodeResource(resources, charen.gtv.projectx.R.drawable.defender_2);
+	}
+
+}
